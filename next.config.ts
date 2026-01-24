fe99ffcc5daf50b5161d919e8210a3262fa83166
QUDDUS <qudduslarek@proton.me>
2026-01-24 23:58:22 +0500

    initialized project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,35 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
+import createMDX from "@next/mdx"
+
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+  options: {
+    jsxImportSource: "@emotion/react",
+  },
+})
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  pageExtensions: ["ts", "tsx", "mdx", "md"],
+  typescript: {
+    tsconfigPath: "./tsconfig.json",
+  },
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [{ key: "Cache-Control", value: "public, s-maxage=60, stale-while-revalidate=120" }],
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+export default withMDX(nextConfig)
---
+++ x-meta.config.ts
@@ -0,0 +1,13 @@
+
+// Sitename
+
+export const XMeta = {
+    siteName: "DocX - lixril",
+    description: "",
+    documentsPath: "content/docs",
+    theme: {
+        provider: [],
+        colorScheme: []
+    },
+    searchProvider: "name"
+}
