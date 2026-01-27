1392c3ed8cb797ef4aed110cc59d5ec5dde0b880
QUDDUS <qudduslarek@proton.me>
2026-01-27 15:20:10 +0500

    modify: config file for dynamic components

--- x-meta.config.ts
+++ x-meta.config.ts
@@ -1,4 +1,6 @@
-
+import { DocPagination } from "./components/doc-pagination"
+import { DocTOC } from "./components/doc-toc"
+import { GButton } from "./plugins/deftheme/GButton"
 // Sitename
 
 export const XMeta = {
@@ -9,5 +11,12 @@ export const XMeta = {
         provider: [],
         colorScheme: []
     },
-    searchProvider: "name"
+    searchProvider: "<void>",
+    interface: {
+        components: {
+            button: GButton,
+            pagination: DocPagination,
+            TOC: DocTOC
+        }
+    }
 }
