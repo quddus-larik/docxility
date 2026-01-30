99e1e91088d75a7a7d5e8c1af2b163fb3e512c00
QUDDUS <qudduslarek@proton.me>
2026-01-30 23:49:12 +0500

    add serialization for pretty codeblock

--- lib/mdx-provider.tsx
+++ lib/mdx-provider.tsx
@@ -1,11 +1,29 @@
-import { MDXRemote } from "next-mdx-remote/rsc"
-import { mdxComponents } from "@/components/mdx-components"
+import { MDXRemote } from "next-mdx-remote/rsc";
+import rehypePrettyCode from "rehype-pretty-code";
+import { mdxComponents } from "@/components/mdx-components";
 
 interface AppMDXProviderProps {
-  source: string
+  source: string;
 }
 
 export function AppMDXProvider({ source }: AppMDXProviderProps) {
-  // no hooks used here
-  return <MDXRemote source={source} components={mdxComponents} />
+  return (
+    <MDXRemote
+      source={source}
+      components={mdxComponents}
+      options={{
+        mdxOptions: {
+          rehypePlugins: [
+            [
+              rehypePrettyCode,
+              {
+                theme: "github-light",
+                keepBackground: false,
+              },
+            ],
+          ],
+        },
+      }}
+    />
+  );
 }
