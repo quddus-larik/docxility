26833d13557d641dcac12be32689053596fbcafe
QUDDUS <qudduslarek@proton.me>
2026-02-11 17:39:02 +0500

    add side bar configurations

--- components/providers.tsx
+++ components/providers.tsx
@@ -3,13 +3,16 @@
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { SearchProvider } from "@/components/search-context";
+import { SidebarCacheProvider } from "@/lib/sidebar-cache-context";
 
 export function Providers({ children, ...props }: React.ComponentProps<typeof NextThemesProvider>) {
   return (
     <NextThemesProvider {...props}>
-      <SearchProvider>
-        {children}
-      </SearchProvider>
+      <SidebarCacheProvider>
+        <SearchProvider>
+          {children}
+        </SearchProvider>
+      </SidebarCacheProvider>
     </NextThemesProvider>
   );
 }
